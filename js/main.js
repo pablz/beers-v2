221e3ca4a2248b273b0d9a31afab9e972ab8924a
pablz <pablo.visual.group@gmail.com>
2020-07-10 08:37:02 -0300

    fix: beersArrs

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 var beersJson = 'https://raw.githubusercontent.com/pablz/logourl/master/beers.json',
 assetsJson = 'https://raw.githubusercontent.com/pablz/logourl/master/assets.json'
 assetsArr = [],
-beersArr = [];
+beersArr = [],
+intro = document.querySelector('#intro'),
+introBg = document.querySelector('.intro-img');
 
 (()=>{
-    getBeers()
+    getBeers();
+    getAssets();
 })();
 
 function getBeers(){
@@ -21,10 +24,38 @@ function getBeers(){
     })
     .finally(()=>{//fynally nos permite hacer algo si todo salió bien
         console.log('Llegó la birra!'); 
+        printBeerList();
+    })
+}
+
+function getAssets(){
+    //consumir un json de modo asíncrono con fetch api
+    fetch(assetsJson)
+    .then(response =>{
+            return response.json();//el método json() extrae el cuerpo del recurso solicitado
+    })
+    .then(data => {//data son los datos del response
+        assetsArr.push(data);
+        console.log(data)
+    })
+    .catch(error => {//captura si hubo un error en la petición
+        console.log(error);
+    })
+    .finally(()=>{//fynally nos permite hacer algo si todo salió bien
+        console.log('Llegaron los assets!'); 
+        setBg();
     })
 }
 
 
+function printBeerList(){
+    console.log(beersArr[0].beers[0].nombre)
+}
 
+function setBg(){
+    for(let item of assetsArr){
+       introBg.src=item.bgs[2].img;
+    }
+}
 
 
